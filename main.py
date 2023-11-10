0e5f15fa7946e968aaa857b0c2231aa883cca6eb
codepythonprojects <113179663+technoya@users.noreply.github.com>
2023-11-10 09:18:40 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import telebot
+
+# Replace 'YOUR_TOKEN' with your actual bot token
+bot = telebot.TeleBot('6831277971:AAFxb3fAHSqSqSIXAROMKFm7xflEBa1uHv4')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello! I am your Telegram bot.")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
