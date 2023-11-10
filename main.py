ecd624c39206e6f54123694f2ec97c9c61b59e4b
codepythonprojects <113179663+technoya@users.noreply.github.com>
2023-11-10 09:22:59 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot('6831277971:AAFxb3fAHSqSqSIXAROMKFm7xflEBa1uHv4')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello! I am your Telegram bot.")
+    bot.reply_to(message, "Hello! I am your Telegram bot. new bot")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
